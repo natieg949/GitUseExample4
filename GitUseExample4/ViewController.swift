82688ac1a116380de38ba8ad71b1802a3d9fd0f7
Natalie Goldberg <natalie.goldberg.949@k12.friscoisd.org>
2020-10-06 11:17:44 -0500

    Log message on app launch

--- GitUseExample4/ViewController.swift
+++ GitUseExample4/ViewController.swift
@@ -16,7 +16,6 @@ class ViewController: UIViewController {
 
     @IBAction func buttonClicked(_ sender: UIButton) {
         print("This is a Git tutorial")
-        print("This is a test")
     }
 }
 
