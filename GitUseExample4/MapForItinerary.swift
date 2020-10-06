640c192f0c1a3744e21b2228c8c2fd44c4bb7181
Natalie Goldberg <natalie.goldberg.949@k12.friscoisd.org>
2020-10-06 11:21:07 -0500

    Log message on app launch

---
+++ GitUseExample4/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample4
+//
+//  Created by Goldberg, Natalie E on 10/6/20.
+//  Copyright © 2020 Goldberg, Natalie E. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
